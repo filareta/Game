56f8d4d7ab4eb5381cc05f2b6aaac209c5e0cdf8
filareta <filareta_yordanova@abv.bg>
2013-02-13 20:13:57 +0200

    Configurations

---
+++ config_file.rb
@@ -0,0 +1,18 @@
+module TomAndJerry
+
+  module Configurations
+    TILE_SIZE_X = 1
+    TILE_SIZE_Y = 1
+    MAP_SIZE_X = 16
+    MAP_SIZE_Y = 12
+    MAPS = ["map01.txt", "map02.txt"]
+    LIVES = 3
+    SCREEN_SIZE_X = 640
+    SCREEN_SIZE_Y = 480
+    FRAMERATE = 30
+    SCALE = 40
+    WALL_X = 40
+    WALL_Y = 40
+  end
+
+end
