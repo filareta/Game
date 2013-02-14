fff0f5392e77ffe4aae47d4ce1de4ab3f9fa8712
filareta <filareta_yordanova@abv.bg>
2013-02-14 15:08:25 +0200

    Renewed

--- game_loop.rb
+++ game_loop.rb
@@ -15,9 +15,14 @@ module TomAndJerry
         if command == 'new'
           game = ConsoleGame.init_game(Configurations::MAPS[0])
         else
-          new_player = game.player_move(command)
-          new_enemies = game.enemies_move
-          game = game.update_game(new_player, new_enemies)
+          if Configurations::DIRECTIONS.include? command
+            new_player = game.player_move(command)
+            new_enemies = game.enemies_move
+            game = game.update_game(new_player, new_enemies)
+          else
+            puts "Invalid command! Try again!"
+            next
+          end
         end
         return if game.over
         if Configurations::MAPS.empty?
